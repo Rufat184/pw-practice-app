b5b5c0b9d3766549f63b0027fc0f0730d2970a70
Rufat <rufat.dadashov@hotmail.com>
2025-08-04 16:30:07 -0400

    updateAir

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,6 +12,14 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  //change default timeout
+  //globalTimeout: 60000,
+  //timeout: 10000,
+
+  //locator timeout
+  expect:{
+    timeout: 2000
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -29,6 +37,8 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    //actionTimeout: 5000,
+    //navigationTimeout: 5000,
     trace: 'on-first-retry',
   },
 
---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,50 @@
+import {expect, test} from '@playwright/test'
+import { timeout } from 'rxjs-compat/operator/timeout'
+
+test.beforeEach(async({page}, testInfo) => {
+    await page.goto('http://uitestingplayground.com/ajax')
+    await page.getByText('Button Triggering AJAX Request').click()
+    testInfo.setTimeout(testInfo.timeout + 2000)
+})
+test ('await method', async({page}) =>{
+    const successBtn = page.locator('.bg-success')
+
+    //await successBtn.click()
+
+    //const text = await  successBtn.textContent()
+    // await successBtn.waitFor({state: 'attached'})
+    // const text = await successBtn.allTextContents()
+
+    // expect(text).toEqual('Data loaded with AJAX get request.')
+
+    await expect(successBtn).toHaveText('Data loaded with AJAX get request.', {timeout:20000})
+})
+
+test ('alternative await methods', async({page}) =>{
+    const successBtn = page.locator('.bg-success')
+    //Wait for element
+    //await page.waitForSelector('.bg-success')
+
+    //Wait for particular response
+    await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+    //Wait for network calls to be completed {not recommended}
+    await page.waitForLoadState('networkidle')
+
+    //Wait 
+    await page.waitForTimeout(5000)
+
+    await page.waitForURL('spirit.com')
+
+    const text = await  successBtn.textContent()
+    expect(text).toContain('Data loaded with AJAX get request.')
+
+})
+
+test ('timeouts', async({page}) =>{
+    const successBtn = page.locator('.bg-success')
+    await successBtn.click()
+
+
+
+})

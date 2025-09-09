dda22719a83c67d981a92957b3699b3f9e7740ab
rufatdadashov <rufat.dadashov@spirit.com>
2025-09-09 10:00:32 -0400

    commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,50 +8,57 @@ require('dotenv').config();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig<TestOptions>({
-  //change default timeout
-  globalTimeout: 30000,
-  timeout: 20000,
+    //change default timeout
+    //globalTimeout: 30000,
+    timeout: 20000,
 
-  //locator timeout
-  expect: {
-    timeout: 2000
-  },
+    //locator timeout
+    expect: {
+        timeout: 2000
+    },
 
-  retries: 0,
-  reporter: 'html',
+    retries: 0,
+    reporter: 'html',
 
-  use: {
-    baseURL: 'http://localhost:4200',
-    globalQAUrl: 'https://www.globalsqa.com/demo-site/draganddrop/',
-    trace: 'on-first-retry',
-    navigationTimeout: 5000,
-    video: {
-      mode: 'off',
-      size: { width: 1920, height: 1080 }
+    use: {
+        baseURL: 'http://localhost:4200',
+        globalQAUrl: 'https://www.globalsqa.com/demo-site/draganddrop/',
+        trace: 'on-first-retry',
+        navigationTimeout: 5000,
+        video: {
+            mode: 'off',
+            size: { width: 1920, height: 1080 }
+        }
+    },
+
+    projects: [
+        // {
+        //   name: 'qat1',
+        //   use: { ...devices['Desktop Chrome'], 
+        //   baseURL: 'http://localhost:4201'  
+        //   },
+        // },
+        // {
+        //   name: 'qat2',
+        //   use: { ...devices['Desktop Chrome'], 
+        //   baseURL: 'http://localhost:4202'  
+        //   },
+        // },
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+
+        {
+            name: 'firefox',
+            use: { browserName: 'firefox' },
+        },
+    ],
+
+    webServer: {
+        command: 'npm run start',
+        url: 'http://localhost:4200',
+        reuseExistingServer: true
     }
-  },
 
-  projects: [
-    // {
-    //   name: 'qat1',
-    //   use: { ...devices['Desktop Chrome'], 
-    //   baseURL: 'http://localhost:4201'  
-    //   },
-    // },
-    // {
-    //   name: 'qat2',
-    //   use: { ...devices['Desktop Chrome'], 
-    //   baseURL: 'http://localhost:4202'  
-    //   },
-    // },
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { browserName: 'firefox' },
-    },
-  ],
 });
--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,13 +1,13 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 import { PageManager } from '../page-objects/pageManager'
-import {faker} from '@faker-js/faker'
+import { faker } from '@faker-js/faker'
 
-test.beforeEach(async({page}) => {
+test.beforeEach(async ({ page }) => {
     await page.goto('/')
-    
+
 })
 
-test ('navigate to from page', async({page}) => {
+test('navigate to from page', async ({ page }) => {
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
     await pm.navigateTo().datePickerPage()
@@ -16,15 +16,15 @@ test ('navigate to from page', async({page}) => {
     await pm.navigateTo().toolTipPage()
 })
 
-test ('parametrized methods', async ({page}) =>{
+test('parametrized methods', async ({ page }) => {
     const pm = new PageManager(page)
     const randomFullName = faker.person.fullName()
     const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`.toLowerCase()
 
     await pm.navigateTo().formLayoutsPage()
     await pm.onFormLayoutPage().submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', "Option 2")
-    await page.screenshot({path: 'screenshots/parametrizedGridForm.png'})
-    await page.locator('nb-card', {hasText: "Inline form"}).screenshot({path: 'screenshots/parametrizedGridFormLocator.png'})
+    await page.screenshot({ path: 'screenshots/parametrizedGridForm.png' })
+    await page.locator('nb-card', { hasText: "Inline form" }).screenshot({ path: 'screenshots/parametrizedGridFormLocator.png' })
     await pm.onFormLayoutPage().submitInlineFormWithNameEmailAndCheckedBox(randomFullName, randomEmail, true)
     // await pm.navigateTo().datePickerPage()
     // await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(10)
